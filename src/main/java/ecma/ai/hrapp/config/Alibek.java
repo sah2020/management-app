2b26712715dfa091487406e85b089dd97ce490eb
AlibekKhudoykulov <alibekxudoyqulovofficial@gmail.com>
2021-05-29 10:59:05 +0500

    new entity

--- src/main/java/ecma/ai/hrapp/config/Alibek.java
+++ src/main/java/ecma/ai/hrapp/config/Alibek.java
@@ -4,8 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Alibek {
+    @Id
+    @GeneratedValue
+    private UUID id;
 }
