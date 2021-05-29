bd2095946b4ff609d412a7b644d0c575b71ad209
AlibekKhudoykulov <alibekxudoyqulovofficial@gmail.com>
2021-05-29 11:01:06 +0500

    new AAA class

---
+++ src/main/java/ecma/ai/hrapp/config/AAA.java
@@ -0,0 +1,20 @@
+package ecma.ai.hrapp.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class AAA {
+    @Id
+    @GeneratedValue
+    private UUID id;
+}
